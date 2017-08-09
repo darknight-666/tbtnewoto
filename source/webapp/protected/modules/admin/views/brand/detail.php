1aff6d521f8be588c28318d885d8ecf6115b15ea
yunlingyin <yunlingyin@yunlingyin-PC>
2017-08-09 18:09:13 +0800

    task

--- source/webapp/protected/modules/admin/views/brand/detail.php
+++ source/webapp/protected/modules/admin/views/brand/detail.php
@@ -2,5 +2,113 @@
 $this->pageTitle = Yii::app()->name . ' - 品牌详情';
 $this->module->params = array('title' => '品牌详情', 'title_img' => 'fa-th', 'icon' => '');
 ?>
+<!--详情-->
+<div class="tbt-panel">
+    <div class="panel-header">
+        <h3 class="panel-title">详情：</h3>
+    </div>
+    <div class="panel-body">
+        <table class="table-view table-striped">
+            <tbody>
+            <tr >
+                <th>客户名称</th>
+                <td></td>
+            </tr>
+            <tr>
+                <th>商户类别</th>
+                <td></td>
+            </tr>
+            <tr>
+                <th>商圈</th>
+                <td></td>
+            </tr>
+            <tr>
+                <th>标签</th>
+                <td></td>
+            </tr>
+            <tr>
+                <th>买单满减</th>
+                <td></td>
+            </tr>
+            </tbody>
+        </table>
+    </div>
+</div>
+<!--银行补贴详情-->
+    <div class="tbt-panel">
+        <div class="panel-header">
+            <h3 class="panel-title">银行补贴详情：</h3>
+        </div>
+        <div class="panel-body">
+            <div class="section">
+                <p></p>
+            </div>
+        </div>
+    </div>
+<!--推存理由-->
+<div class="tbt-panel">
+    <div class="panel-header">
+        <h3 class="panel-title">推存理由：</h3>
+    </div>
+    <div class="panel-body">
+        <div class="section">
+            <p></p>
+        </div>
+    </div>
+</div>
+<!--推存理由详情-->
+<div class="tbt-panel">
+    <div class="panel-header">
+        <h3 class="panel-title">推存理由详情：</h3>
+    </div>
+    <div class="panel-body">
+        <div class="section">
+            <p></p>
+        </div>
+    </div>
+</div>
+<!-- 提供增值服务 -->
+<div class="tbt-panel">
+    <div class="panel-header">
+        <h3 class="panel-title">提供增值服务：</h3>
+    </div>
+    <div class="panel-body">
+        <div class="section">
+            <div class="from-group">
+                <p class="control-label" >提供增值服务：</p>
+                <div class="from-control col-lg">
+                    <div>
+                        <input   name="TrainingCourse[name]"  type="checkbox" >  <div class="sep"></div> <span>咖啡设备 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; </span>
+                        <input   name="TrainingCourse[name]"  type="checkbox" > <div class="sep"></div> <span>咖啡设备  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; </span>
+                        <input   name="TrainingCourse[name]"  type="checkbox" >  <div class="sep"></div> <span>咖啡设备  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; </span>
+                        <input   name="TrainingCourse[name]"  type="checkbox" > <div class="sep"></div> <span>咖啡设备</span>
 
-
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!--品牌主图-->
+<div class="tbt-panel">
+    <div class="panel-header">
+        <h3 class="panel-title">品牌主图：</h3>
+    </div>
+    <div class="panel-body">
+        <div class="section">
+            <img src="#">
+        </div>
+    </div>
+</div>
+<!--品牌主图-->
+<div class="tbt-panel">
+    <div class="panel-header">
+        <h3 class="panel-title">品牌主图：</h3>
+    </div>
+    <div class="panel-body">
+        <div class="section">
+            <img src="#">
+            <img src="#">
+        </div>
+    </div>
+</div>
