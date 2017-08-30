5f6603f58daa5316618e830ba0a1d671b4df00f7
admin <oc_wangmingxiu@tongbaotu.com>
2017-08-30 18:11:24 +0800

    repush

--- source/webapp/protected/config/params_default.php
+++ source/webapp/protected/config/params_default.php
@@ -2,6 +2,6 @@
 
 return array(
 //    'sms' => require_once(dirname(__FILE__) . '/params_sms_local.php'),
-    'site' => require_once(dirname(__FILE__) . '/params_site_local.php'),
-    'sms' => require_once(dirname(__FILE__) . '/params_sms_local.php'),
+    'site' => require_once(dirname(__FILE__) . '/params_site_default.php'),
+    'sms' => require_once(dirname(__FILE__) . '/params_sms_default.php'),
 );
