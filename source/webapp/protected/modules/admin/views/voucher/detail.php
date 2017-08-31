314c49c2969f11ca23a4637f15f4f222df3622c5
yunlingyin <yunlingyin@yunlingyin-PC>
2017-08-31 18:03:05 +0800

    task

--- source/webapp/protected/modules/admin/views/voucher/detail.php
+++ source/webapp/protected/modules/admin/views/voucher/detail.php
@@ -2,3 +2,75 @@
 $this->pageTitle = Yii::app()->name . ' - 代金券详情';
 $this->module->params = array('title' => '代金券详情', 'title_img' => 'fa-th', 'icon' => '');
 ?>
+<!--订单详情-->
+<div class="tbt-panel">
+    <div class="panel-header">
+        <h3 class="panel-title">订单详情</h3>
+    </div>
+    <div class="panel-body">
+        <table class="table-view table-striped">
+            <tbody>
+            <tr>
+                <th>客户</th>
+                <td></td>
+            </tr>
+            <tr>
+                <th>代金券名称</th>
+                <td></td>
+            </tr>
+            <tr>
+                <th>有效期至</th>
+                <td></td>
+            </tr>
+            <tr>
+                <th>一句描述</th>
+                <td></td>
+            </tr>
+            <tr>
+                <th>数量</th>
+                <td> 张</td>
+            </tr>
+            <tr>
+                <th>支付方式</th>
+                <td></td>
+            </tr>
+            <tr>
+                <th>代金券面值</th>
+                <td></td>
+            </tr>
+            <tr>
+                <th>价格</th>
+                <td></td>
+            </tr>
+            <tr>
+                <th>使用提示</th>
+                <td></td>
+            </tr>
+            <tr>
+                <th>使用门店</th>
+                <td></td>
+            </tr>
+            <tr>
+                <th>收款账户信息</th>
+                <td></td>
+            </tr>
+            <tr>
+                <th>开户户名</th>
+                <td></td>
+            </tr>
+            <tr>
+                <th>账号</th>
+                <td></td>
+            </tr>
+            <tr>
+                <th>开户行名称</th>
+                <td></td>
+            </tr>
+            <tr>
+                <th>账户地址</th>
+                <td></td>
+            </tr>
+            </tbody>
+        </table>
+    </div>
+</div>
